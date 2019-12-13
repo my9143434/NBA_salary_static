006744eab48e989518a3bf9cbcc86c0ec73db0de
YI-TING LIN <my9143434@gmail.com>
2019-12-12 19:04:16 -0800

    Add files via upload

---
+++ lin_yiting_milestone3.py
@@ -0,0 +1,13 @@
+import script
+import script3
+import script2
+
+
+def main():
+    print("Start all scripting")
+    print("\nFinished all scripting process.")
+
+
+if __name__ == "__main__":
+    main()
+
